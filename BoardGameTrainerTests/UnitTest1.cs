dc447f0881d423c811d884bbd4141ca92c19c4c4
ZuzaTyszka <zuzia.tyszka@vp.pl>
2022-11-25 22:32:28 +0100

    dodane projekty testowe

---
+++ BoardGameTrainerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BoardGameTrainerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Game.OthelloTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Game.OthelloTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
