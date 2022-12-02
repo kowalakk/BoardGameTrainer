465ecd4609c009d74a512de6f8e583560d08689a
ZuzaTyszka <zuzia.tyszka@vp.pl>
2022-12-02 14:33:54 +0100

    dodany typ Unit, implementacja Othello state

--- Game.Othello/OthelloAction.cs
+++ Game.Othello/OthelloAction.cs
@@ -2,9 +2,18 @@
 {
     public class OthelloAction : IEquatable<OthelloAction>
     {
+        public (int, int) Position { get; set; }
+        public OthelloState.Field FieldContent { get; set; }
+
         public bool Equals(OthelloAction? other)
         {
-            throw new NotImplementedException();
+            if (this == null || other == null) 
+                return false; 
+            if (this.Position != other.Position)
+                return false;
+            if (this.FieldContent!= other.FieldContent) 
+                return false;
+            return true;
         }
     }
 }
