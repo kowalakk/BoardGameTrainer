986cb296c25c25581f15bbeffc0421ef2bfdd566
kowalakk <konradkowalak@gmail.com>
2022-12-04 20:36:42 +0100

    More tests

---
+++ Game.CheckersTest/CheckersStateTests.cs
@@ -0,0 +1,49 @@
+using Game.Checkers;
+
+namespace Game.CheckersTest
+{
+    public class CheckersStateTests
+    {
+        public static Piece[,] emptyBoard = new Piece[,]
+            {
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+                { Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None, Piece.None },
+            };
+        [Fact]
+        public void EqualsShouldBeTrue()
+        {
+            CheckersState state1 = new CheckersState();
+            CheckersState state2 = new CheckersState(state1);
+            Assert.True(state1.Equals(state2));
+            Assert.True(state2.Equals(state1));
+        }
+        [Fact]
+        public void NotEqualPlayersShouldBeFalse()
+        {
+            CheckersState state1 = new CheckersState(emptyBoard, Player.White);
+            CheckersState state2 = new CheckersState(emptyBoard, Player.Black);
+            Assert.False(state1.Equals(state2));
+            Assert.False(state2.Equals(state1));
+        }
+        [Fact]
+        public void NotEqualBoardsShouldBeFalse()
+        {
+            CheckersState state1 = new CheckersState();
+            CheckersState state2 = new CheckersState(emptyBoard, Player.White);
+            Assert.False(state1.Equals(state2));
+            Assert.False(state2.Equals(state1));
+        }
+        [Fact]
+        public void EqualsWithNullShouldBeFalse()
+        {
+            CheckersState state = new CheckersState();
+            Assert.False(state.Equals(null));
+        }
+    }
+}
---
+++ Game.CheckersTest/PossibleActionsTests.cs
@@ -0,0 +1,15 @@
+﻿using Game.Checkers;
+
+namespace Game.CheckersTest
+{
+    public class PossibleActionsTests
+    {
+        [Fact]
+        public void PossibleActionsForEmptyBoardShouldBeEmpty()
+        {
+            CheckersState state = new CheckersState(CheckersStateTests.emptyBoard, Player.White);
+            var possibleActions = PossibleActions(state);
+            Assert.Empty(possibleActions);
+        }
+    }
+}
