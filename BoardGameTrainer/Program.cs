bf7de06a4613549d1b85e434021aa3d950af8086
ZuzaTyszka <zuzia.tyszka@vp.pl>
2023-01-08 22:05:02 +0100

    wyświetlanie przykładowego obrazka

--- BoardGameTrainer/Program.cs
+++ BoardGameTrainer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Checkers;
 using Game.IGame;
+using Gdk;
 using Gtk;
 
 namespace BoardGameTrainer
@@ -15,36 +16,55 @@ namespace BoardGameTrainer
         public static void Main(string[] args)
         {
             Application.Init();
-            string title = "New Game";
+            
             var app = new Application("x.y.z", GLib.ApplicationFlags.None);
             app.Register(GLib.Cancellable.Current);
-            var win = new Window(Gtk.WindowType.Toplevel);
+            var mainWindow = createMainWindow();
+            app.AddWindow(mainWindow);
+            
+            Application.Run();
+        }
+
+        public static Gtk.Window createMainWindow()
+        {
+            string title = "New Game";
+            var win = new Gtk.Window(Gtk.WindowType.Toplevel);
             win.DefaultSize = new Gdk.Size(700, 500);
-            app.AddWindow(win);
+            
             var mainVBox = new Gtk.VBox();
             var panelHbox = new Gtk.HBox();
-            var contentVBox = new Gtk.VBox();
+            var titleAndContentVBox = new Gtk.VBox();
+
+            var contentHBox = new Gtk.HBox();
+            var boardPixbuf = new Gdk.Pixbuf("..\\..\\..\\Tulips.jpg", 200, 200);
+
+            // zamiast tego będzie przypisanie do boardImage wartości game.drawBoard()
+            var boardImage = new Gtk.Image(boardPixbuf);
+            contentHBox.PackStart(boardImage, false, false, 0);
+            boardImage.Show();
+
             var newGameButton = new Button("New Game");
             var restartButton = new Button("Restart");
             panelHbox.PackStart(newGameButton, false, false, 0);
             panelHbox.PackStart(restartButton, false, false, 0);
 
             var mainTitle = new Gtk.Label(title);
-            contentVBox.PackStart(mainTitle, false, false, 0);
+            titleAndContentVBox.PackStart(mainTitle, false, false, 0);
+            titleAndContentVBox.PackStart(contentHBox, false, false, 0);
             mainTitle.Show();
+            contentHBox.Show();
 
             mainVBox.PackStart(panelHbox, false, false, 0);
             panelHbox.Show();
-            mainVBox.PackStart(contentVBox, false, false, 0);
-            contentVBox.Show();
+            mainVBox.PackStart(titleAndContentVBox, false, false, 0);
+            titleAndContentVBox.Show();
             win.Add(mainVBox);
             mainVBox.Show();
             newGameButton.Show();
             restartButton.Show();
             win.Show();
             win.DeleteEvent += (sender, args) => Application.Quit();
-            //newGameButton.Clicked += (sender, args) => Application.Quit();
-            Application.Run();
+            return win;
         }
     }
 }
