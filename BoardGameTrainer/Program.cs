cfaf179295f91231dc1dcd1efe70099c6875606c
ZuzaTyszka <zuzia.tyszka@vp.pl>
2023-01-08 13:26:15 +0100

    window layout

--- BoardGameTrainer/Program.cs
+++ BoardGameTrainer/Program.cs
@@ -15,18 +15,35 @@ namespace BoardGameTrainer
         public static void Main(string[] args)
         {
             Application.Init();
-            
+            string title = "New Game";
             var app = new Application("x.y.z", GLib.ApplicationFlags.None);
             app.Register(GLib.Cancellable.Current);
-            var win = new GameWindow(Gdk.WindowType.Toplevel);
-            app.AddWindow(win);
+            var win = new Window(Gtk.WindowType.Toplevel);
             win.DefaultSize = new Gdk.Size(700, 500);
-            var button = new Button("QUIT");
-            win.Add(button);
-            button.Show();
+            app.AddWindow(win);
+            var mainVBox = new Gtk.VBox();
+            var panelHbox = new Gtk.HBox();
+            var contentVBox = new Gtk.VBox();
+            var newGameButton = new Button("New Game");
+            var restartButton = new Button("Restart");
+            panelHbox.PackStart(newGameButton, false, false, 0);
+            panelHbox.PackStart(restartButton, false, false, 0);
+
+            var mainTitle = new Gtk.Label(title);
+            contentVBox.PackStart(mainTitle, false, false, 0);
+            mainTitle.Show();
+
+            mainVBox.PackStart(panelHbox, false, false, 0);
+            panelHbox.Show();
+            mainVBox.PackStart(contentVBox, false, false, 0);
+            contentVBox.Show();
+            win.Add(mainVBox);
+            mainVBox.Show();
+            newGameButton.Show();
+            restartButton.Show();
             win.Show();
             win.DeleteEvent += (sender, args) => Application.Quit();
-            button.Clicked += (sender, args) => Application.Quit();
+            //newGameButton.Clicked += (sender, args) => Application.Quit();
             Application.Run();
         }
     }
