d9c7d500809371940433ffe23ae8fed464608271
kowalakk <konradkowalak@gmail.com>
2023-06-29 00:08:56 +0200

    Revert "Revert "Refactoring""
    This reverts commit 68452bd5380e425987cea910c598ad780e5b3d2c.

--- BoardGameTrainer/CustomWidgets/PlayersBox.cs
+++ BoardGameTrainer/CustomWidgets/PlayersBox.cs
@@ -5,20 +5,27 @@ namespace BoardGameTrainer
 {
     internal class PlayersBox : VBox
     {
-        private readonly CheckButtonsFrame numOfPlayersFrame = new("Human players");
-        private readonly CheckButtonsFrame showHintsFrame = new("Show hints");
+        private readonly CheckButtonsFrame numOfPlayersFrame;
+        private readonly CheckButtonsFrame showHintsFrame;
         public PlayersBox(Dictionary<Player, bool> humanPlayers, Dictionary<Player, bool> showHints)
         {
-            numOfPlayersFrame.FirstClicked += (sender, args) => { humanPlayers[Player.One] = numOfPlayersFrame.FirstActive; };
-            numOfPlayersFrame.SecondClicked += (sender, args) => { humanPlayers[Player.Two] = numOfPlayersFrame.SecondActive; };
+            numOfPlayersFrame = new("Human players", humanPlayers);
             numOfPlayersFrame.Show();
 
-            showHintsFrame.FirstClicked += (sender, args) => { showHints[Player.One] = showHintsFrame.FirstActive; };
-            showHintsFrame.SecondClicked += (sender, args) => { showHints[Player.Two] = showHintsFrame.SecondActive; };
+            showHintsFrame = new("Show hints", showHints);
             showHintsFrame.Show();
 
             PackStart(numOfPlayersFrame, false, false, 3);
             PackStart(showHintsFrame, false, false, 3);
+
+            numOfPlayersFrame.FirstCheckbox.Clicked += (sender, args) =>
+            {
+                showHintsFrame.FirstCheckbox.Sensitive = numOfPlayersFrame.FirstCheckbox.Active;
+            };
+            numOfPlayersFrame.SecondCheckbox.Clicked += (sender, args) =>
+            {
+                showHintsFrame.SecondCheckbox.Sensitive = numOfPlayersFrame.SecondCheckbox.Active;
+            };
         }
     }
 }
